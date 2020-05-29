7abb2862e43bba03506faccd27a5d18b953140f0
Anderson Kruel <anderson.kruel@gmail.com>
2020-05-29 03:10:43 -0300

    projeto funcional

---
+++ index.js
@@ -0,0 +1,106 @@
+var users = [];
+var titleFoundUsers = document.querySelector('#titleFoundUsers');
+var titleUserStatic = document.querySelector('#titleUserStatic');
+var divFoundUsers = document.querySelector('#foundUsers');
+var divUserStatic = document.querySelector('#userStatic');
+var search = document.querySelector('#search');
+var btnSearch = document.querySelector('#btnSearch');
+
+window.onload = () => {
+  resetFields();
+
+  const API_URL =
+    'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo';
+
+  const api = fetch(API_URL).then((response) => response.json());
+
+  api.then((response) => {
+    response.results.map((user) => {
+      reducedUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        gender: user.gender,
+        age: user.dob.age,
+        picture: user.picture,
+      };
+      users.push(reducedUser);
+    });
+  });
+};
+
+resetFields = () => {
+  titleFoundUsers.innerHTML = 'Nenhum usuário filtrado!';
+  titleUserStatic.innerHTML = 'Nada a ser exibido!';
+  divFoundUsers.innerHTML = '';
+  divUserStatic.innerHTML = '';
+  foundUsers = [];
+};
+
+findUsers = (event) => {
+  controlButton();
+  resetFields();
+  if ((event && event.keyCode == 13 && search != '') || (!event && search != '')) {
+    let foundUserAges = null;
+    let username = search.value.toLowerCase();
+
+    let foundUsers = users.filter((user) => {
+      return user.name.toLowerCase().includes(username);
+    });
+
+    let foundUsersMale = foundUsers.filter((user) => {
+      return user.gender === 'male';
+    });
+
+    let foundUsersFemale = foundUsers.filter((user) => {
+      return user.gender === 'female';
+    });
+
+    titleFoundUsers.innerHTML = `${foundUsers.length} usuário(s) encontrado(s)!`;
+    titleUserStatic.innerHTML = `Estatísticas`;
+
+    if (foundUsers.length > 0) {
+      foundUsers.forEach((element) => {
+        foundUserAges += element.age;
+        divFoundUsers.innerHTML += `${getUser(element)}`;
+      });
+
+      let averAge = foundUserAges / foundUsers.length;
+
+      divUserStatic.innerHTML = getUserStatic(
+        foundUsersMale,
+        foundUsersFemale,
+        foundUserAges,
+        averAge,
+      );
+    } else {
+      resetFields();
+    }
+  } else {
+    resetFields();
+  }
+};
+
+getUserStatic = (foundUsersMale, foundUsersFemale, foundUserAges, averAge) => {
+  return `<p> Sexo Masculino: ${foundUsersMale.length}</p>
+          <p> Sexo Feminino: ${foundUsersFemale.length}</p>
+          <p> Soma das idades: ${foundUserAges}</p>
+          <p> Média das idades: ${parseFloat(averAge).toPrecision(4)}</p>`;
+};
+
+getUser = (user) => {
+  return `<div class="row">
+            <div col s12>
+               <div class="userDetails">
+                  <img src=${user.picture.thumbnail} /> 
+                  <div>${user.name}, ${user.age} anos </div>
+               </div>
+            </div>
+          </div>`;
+};
+
+controlButton = () => {
+  if (search.value == '') {
+    btnSearch.className = 'waves-effect waves-light btn disabled';
+  } else {
+    btnSearch.className = 'waves-effect waves-light btn';
+  }
+};
